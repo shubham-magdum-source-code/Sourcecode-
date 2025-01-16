72cb8b317c805760cfb109f4551c46216ae659df
ShrutikaMagdum <shrutikamagdum1919@gmail.com>
2025-01-16 20:19:40 +0530

    new

---
+++ practice 01.js
@@ -0,0 +1,12 @@
+// let date = new Date()
+// let curday = date.getDate()
+// let curmonth = date.getMonth()+1
+// let curyear = date.getFullYear()
+// let curhour = date.getHours()
+// let curminute = date.getMiutes()
+// let format = curhour%12
+// let ampm = curhour>12?"PM":"AM"
+// console.log(`${curday}/${curmonth}/${curyear}/${format}:${curminute}${ampm}`)
+
+
+ //console.log(`${curday}/${curmonth}/${curyear}/${format}:${curminute}${ampm}`)
